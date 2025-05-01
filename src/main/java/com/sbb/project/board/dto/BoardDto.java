91fbfb6e4b4aeb2f0e67f83018a8915ca9e71741
kael0595 <changgyu549@gmail.com>
2025-05-01 18:49:10 +0900

    BoardDto 작성

---
+++ src/main/java/com/sbb/project/board/dto/BoardDto.java
@@ -0,0 +1,13 @@
+package com.sbb.project.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardDto {
+
+    private String title;
+
+    private String content;
+}
