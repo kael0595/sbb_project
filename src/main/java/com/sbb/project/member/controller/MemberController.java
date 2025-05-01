f9f2bf95723a419d465faa8d099c00a23022cb4e
kael0595 <changgyu549@gmail.com>
2025-05-01 18:19:45 +0900

    마이페이지 작성

--- src/main/java/com/sbb/project/member/controller/MemberController.java
+++ src/main/java/com/sbb/project/member/controller/MemberController.java
@@ -7,8 +7,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -48,5 +50,12 @@ public class MemberController {
     @PostMapping("/logout")
     public String logout() {
         return "redirect:/";
+    }
+
+    @GetMapping("/mypage/{username}")
+    public String mypage(@PathVariable("username") String username, Model model) {
+        Member member = memberService.getMemberByusername(username);
+        model.addAttribute("member", member);
+        return "member/mypage";
     }
 }
