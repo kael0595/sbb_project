96ce30161a0161ab42c5cf0d74830dbf14443e31
kael0595 <changgyu549@gmail.com>
2025-05-01 15:48:38 +0900

    @SuperBuilder 추가 및 누락된 어노테이션 추가

--- src/main/java/com/sbb/project/base/entity/BaseEntity.java
+++ src/main/java/com/sbb/project/base/entity/BaseEntity.java
@@ -1,26 +1,31 @@
 package com.sbb.project.base.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @MappedSuperclass
+@SuperBuilder(toBuilder = true)
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreatedDate
     private LocalDateTime createDt;
 
+    @LastModifiedDate
     private LocalDateTime updateDt;
 }
