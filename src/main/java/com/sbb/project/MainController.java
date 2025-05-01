ebf5f983298890e2ac9861dcfd641af1ca97d147
kael0595 <changgyu549@gmail.com>
2025-05-01 18:15:01 +0900

    메인페이지 출력 방식 변경 및 메인 컨트롤러 로그인 정보 호출 방식 변경

--- src/main/java/com/sbb/project/MainController.java
+++ src/main/java/com/sbb/project/MainController.java
@@ -3,6 +3,9 @@ package com.sbb.project;
 import com.sbb.project.member.entity.Member;
 import com.sbb.project.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +14,19 @@ import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MainController {
 
     private final MemberService memberService;
 
     @GetMapping("/")
     public String index(Model model,
-                        Principal principal) {
+                        @AuthenticationPrincipal User member) {
 
-        Member member = memberService.getMemberByusername(principal.getName());
-
-        model.addAttribute("member", member);
+        if (member != null) {
+            Member loginMember = memberService.getMemberByusername(member.getUsername());
+            model.addAttribute("member", loginMember);
+        }
 
         return "index";
     }
